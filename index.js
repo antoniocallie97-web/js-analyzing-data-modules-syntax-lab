71074e239eeff126a5574bc1bfa4075c72d90f6d
antoniocallie97-web <antoniocallie97@gmail.com>
2026-04-17 14:06:54 +0300

    analyzing data modules syntax lab one

--- index.js
+++ index.js
@@ -5,26 +5,13 @@ function combineUsers(...args) {
         users: []
     };
 
-    // Merge all arrays
     args.forEach(array => {
         combinedObject.users.push(...array);
     });
 
-    // Add merge date
     combinedObject.merge_date = new Date().toString('M/d/yyyy');
 
     return combinedObject;
 }
 
-// Export for testing (important for labs)
 module.exports = combineUsers;
-
-
-// Example usage (you can remove this if tests fail)
-const result = combineUsers(
-    ['john', 'mary'],
-    ['sam', 'lisa'],
-    ['peter']
-);
-
-console.log(result);
