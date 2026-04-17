47c1bfd150981f1a1498ff95c1d05435805ab5ac
antoniocallie97-web <antoniocallie97@gmail.com>
2026-04-17 13:59:10 +0300

    analyzing data modules syntax lab

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+require('datejs');
+
+function combineUsers(...args) {
+    const combinedObject = {
+        users: []
+    };
+
+    // Merge all arrays
+    args.forEach(array => {
+        combinedObject.users.push(...array);
+    });
+
+    // Add merge date
+    combinedObject.merge_date = new Date().toString('M/d/yyyy');
+
+    return combinedObject;
+}
+
+// Export for testing (important for labs)
+module.exports = combineUsers;
 
 
+// Example usage (you can remove this if tests fail)
+const result = combineUsers(
+    ['john', 'mary'],
+    ['sam', 'lisa'],
+    ['peter']
+);
 
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
+console.log(result);
